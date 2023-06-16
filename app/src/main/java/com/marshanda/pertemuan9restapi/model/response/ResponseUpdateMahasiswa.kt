57a0038574bb27abd24b2c65180fcc5b3248dfc9
Marshanda Raihan Widyaswari <20102020@ittelkom-pwt.ac.id>
2023-06-16 12:56:49 +0700

    pertemuan10

---
+++ app/src/main/java/com/marshanda/pertemuan9restapi/model/response/ResponseUpdateMahasiswa.kt
@@ -0,0 +1,11 @@
+package com.marshanda.pertemuan9restapi.model.response
+
+
+import com.google.gson.annotations.SerializedName
+
+data class ResponseUpdateMahasiswa(
+    @SerializedName("data")
+    val data: String,
+    @SerializedName("status")
+    val status: String
+)
